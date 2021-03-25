edfcb64e20081516c1e3a4642edd9a23dd71eba1
TPrice22 <trlfprice@gmail.com>
2021-03-24 20:13:24 -0400

    New Services Page and JavaScript

---
+++ js/services.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+    var inputs = document.getElementsByClassName('sum'),
+        total  = document.getElementById('payment-total');
+    
+     for (var i=0; i < inputs.length; i++) {
+        inputs[i].onchange = function() {
+            var add = this.value * (this.checked ? 1 : -1);
+            total.innerHTML = parseFloat(total.innerHTML) + add
+            var new_total = parseFloat(document.getElementById('input').value);
+          console.log(new_total);
+            document.getElementById('input').value=new_total + add
+        }
+      }
+    }
+    
